7384533cec75c7b87d09305325624ed6f633ed1c
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2022-01-13 10:41:53 +0100

    Initial project

---
+++ src/main/java/com/example/workshoprest/WorkShopRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshoprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShopRestApplication.class, args);
+    }
+
+}
